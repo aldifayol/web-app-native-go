efb175a04d2c750a76e33fece3874dc8655f79b7
Aldi <aldifayol@gmail.com>
2025-08-07 01:14:10 +0800

    feat: templating parseGlob()

--- main.go
+++ main.go
@@ -7,10 +7,15 @@ import (
 	"path"
 )
 
+type M map[string]interface{}
+
+// later unused
+/*
 func handlerIndex(w http.ResponseWriter, r *http.Request) {
 	message := "welcome"
 	w.Write([]byte(message))
 }
+*/
 
 func handlerHello(w http.ResponseWriter, r *http.Request) {
 	message := "hello world"
@@ -41,10 +46,35 @@ func handlerHtml(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {	
+	// chapter 4
+	var tmpl, err = template.ParseGlob(("views/*"))
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// chapter 3
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("assets"))))
+
+	http.HandleFunc("/index", func(w http.ResponseWriter, r *http.Request) {
+		var data = M{"name": "Iron Man"}
+		err := tmpl.ExecuteTemplate(w, "index", data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError )
+		}
+	})
+
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		var data = M{"name": "Tony Stark"}
+		err := tmpl.ExecuteTemplate(w, "about", data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError )
+		}
+	})
+
 	// chapter 1
 
 	http.HandleFunc("/", handlerHtml)
-	http.HandleFunc("/index", handlerIndex)
+	// http.HandleFunc("/index", handlerIndex)
 	http.HandleFunc("/hello", handlerHello)
 
 	// chapter 2
@@ -54,16 +84,8 @@ func main() {
 		w.Write([]byte(data))
 	})
 
-	// chapter 3
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("assets"))))
-
-	// chapter 4
-
-	address := "localhost:8080"
+	address := ":8080"
 	fmt.Printf("server started at %s\n", address)
-	err :=	http.ListenAndServe(address, nil) 
-	if err != nil {
-		fmt.Println(err.Error())
-	}
+	http.ListenAndServe(address, nil) 
 
 }
